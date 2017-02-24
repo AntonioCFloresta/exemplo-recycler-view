347a3726877d7931ed7f5f313b71e53a9ad56f71
Guilherme Souza <guilherme.matheus.de.souza@gmail.com>
2017-02-24 19:30:20 -0300

    Base do CursoItemRecyclerAdapter

---
+++ app/src/main/java/br/com/senai/guilherme/exemplorecyclerview/CursoItemRecyclerAdapter.java
@@ -0,0 +1,21 @@
+package br.com.senai.guilherme.exemplorecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class CursoItemRecyclerAdapter extends RecyclerView.Adapter<CursoItemViewHolder> {
+    @Override
+    public CursoItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CursoItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
