a66eb60fbb79430dc96126f673b435a4b88d6b31
Guilherme Souza <guilherme.matheus.de.souza@gmail.com>
2017-02-24 19:59:58 -0300

    Metodos do adapter recuperando dados da lista de cursos

--- app/src/main/java/br/com/senai/guilherme/exemplorecyclerview/CursoItemRecyclerAdapter.java
+++ app/src/main/java/br/com/senai/guilherme/exemplorecyclerview/CursoItemRecyclerAdapter.java
@@ -1,21 +1,34 @@
 package br.com.senai.guilherme.exemplorecyclerview;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class CursoItemRecyclerAdapter extends RecyclerView.Adapter<CursoItemViewHolder> {
+    private List<CursoItem> mLista;
+
+    public CursoItemRecyclerAdapter(List<CursoItem> lista) {
+        mLista = lista;
+    }
+
     @Override
     public CursoItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_curso_item, parent, false);
+        return new CursoItemViewHolder(layoutView);
     }
 
     @Override
     public void onBindViewHolder(CursoItemViewHolder holder, int position) {
-
+        CursoItem cursoItem = mLista.get(position);
+        holder.textViewTitulo.setText(cursoItem.getTitulo());
+        holder.textViewTurno.setText(cursoItem.getTurno());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mLista.size();
     }
 }
