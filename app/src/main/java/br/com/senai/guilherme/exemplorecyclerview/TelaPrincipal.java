64e3859b0d9f6cb689ed6253e2f3e0e11b07fc4f
Guilherme Souza <guilherme.matheus.de.souza@gmail.com>
2017-02-24 19:16:22 -0300

    Criados os objetos da layout e definido gerenciador do recyclerView

--- app/src/main/java/br/com/senai/guilherme/exemplorecyclerview/TelaPrincipal.java
+++ app/src/main/java/br/com/senai/guilherme/exemplorecyclerview/TelaPrincipal.java
@@ -2,6 +2,8 @@ package br.com.senai.guilherme.exemplorecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class TelaPrincipal extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        RecyclerView recyclerViewCursos = (RecyclerView) findViewById(R.id.recyclerViewCursos);
+        recyclerViewCursos.setLayoutManager(new LinearLayoutManager(this));
     }
 }
